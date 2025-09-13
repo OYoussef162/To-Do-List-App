5b615e3781f1c8b4a86544d7754f4b8060103306
Omar Youssef <omaryoussef1621999@gmail.com>
2025-09-13 22:26:30 +0300

    Initialization

---
+++ index.js
@@ -0,0 +1,20 @@
+import { initListeners } from "./scripts/eventListeners.js";
+import { initDataOnStartup } from "./scripts/utils.js";
+
+initDataOnStartup();
+initListeners();
+
+/*
+  - Dark Theme
+      ToggleDarkMode ✅
+
+  - Tasks
+      addTask ✅
+      saveToDB ✅
+      renderTaskList (put it in DOM) ✅
+      deleteTask ✅
+      initDataOnStartup ✅
+      Empty State ✅
+      toggleTask ✅
+      toggleCompletedTask ✅
+*/
